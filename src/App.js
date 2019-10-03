733dfd8e6c5a944501c36b541267a9e421d13854
sidd82 <psiddhesh14@gmail.com>
2019-10-03 23:48:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("Requested Started");
+    axios
+      .post(
+        `https://sptapiservice.evershinetechsolutions.com/API/AirSearch`,
+        {
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8",
+            "Access-Control-Allow-Origin": "*"
+          }
+        },
+        {
+          withCredentials: true
+        },
+        {
+          AdultCount: 1,
+          ChildCount: 0,
+          InfantCount: 0,
+          IsDomestic: true,
+          DirectFlight: false,
+          CabinType: 1,
+          AirTripType: 1,
+          Segments: [
+            {
+              Origin: "MAA",
+              Destination: "DEL",
+              DepartureDateTime: "2019-10-04T00:00:00"
+            }
+          ],
+          AirlineSource: []
+        },
+        {
+          auth: {
+            username: "SPTCoreCelo",
+            password: "SPTc@reCel@1920"
+          }
+        }
+      )
+      .then(res => {
+        console.log(res);
+        console.log("Requested Ended");
+      })
+      .catch(e => {
+        console.log(e);
+        console.log("Catch Block Triggered");
+      });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
